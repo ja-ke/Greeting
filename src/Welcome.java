e88d4c2ec020cda554d0425cdfc53e4ae44e792f
jake <ja.ke@me.com>
2012-10-06 22:51:43 -0700

    Added a loop to print 5 times

--- src/Welcome.java
+++ src/Welcome.java
@@ -7,6 +7,8 @@
  */
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
     }
 }
