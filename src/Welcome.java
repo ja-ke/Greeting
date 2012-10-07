1ff0cda546f021d7576966738965cc97d35e6536
jake <ja.ke@me.com>
2012-10-06 22:46:44 -0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jake
+ * Date: 10/6/12
+ * Time: 5:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
